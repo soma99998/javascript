6ea5f43f759312ff1c553c38b308f37f8ad4842f
asmaa elbatanony <asmaa.elbatanony@yahoo.com>
2025-03-02 12:07:56 +0200

    lesson4 and 5

---
+++ lesson4/script.js
@@ -0,0 +1,12 @@
+" use strict";
+const now = 2077;
+const ageJonas = now - 1990;
+console.log(ageJonas);
+const ageSarah = now - 1990;
+console.log(ageSarah);
+console.log(ageJonas * 2, ageJonas / 4, 2 ** 3);
+
+const firstName = "asmaa";
+const lastName = "elbatanony";
+
+console.log(firstName + " " + lastName);
