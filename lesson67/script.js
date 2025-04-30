beda92db4b002ff19cc539b5bae004e4e561c1a0
asmaa elbatanony <asmaa.elbatanony@yahoo.com>
2025-04-30 18:32:38 +0300

    lesson71 added

--- lesson67/script.js
+++ lesson67/script.js
@@ -2,8 +2,22 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log();
+  console.log(firstName);
+  function printAge() {
+    const output = ` ${firstName}you are ${age} , born in ${birthYear}`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var milllenial = true;
+      const str = `you are milllenial ${firstName}`;
+      console.log(str);
+    }
+    //console.log(str);
+    console.log(milllenial);
+  }
+  printAge();
   return age;
 }
 const firstName = "joans";
 calcAge(1990);
+//console.log(age);
+//printAge();
